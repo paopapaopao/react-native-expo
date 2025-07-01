52e955b6cd21351fe420063e99dd3e9372cd2845
paopapaopao <79187858+paopapaopao@users.noreply.github.com>
2025-07-01 21:51:22 +0800

    [Feature] Create Home screen (#2)

---
+++ src/app/index.tsx
@@ -0,0 +1,39 @@
+import { JSX, useEffect, useState } from 'react';
+import { FlatList, StyleSheet, View } from 'react-native';
+
+import { PostCard } from '../components';
+import type { DummyJsonPost } from '../types';
+
+const Index = (): JSX.Element => {
+  const [posts, setPosts] = useState<DummyJsonPost[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        'https://dummyjson.com/posts?limit=0&select=id,title,body'
+      );
+
+      const data: { posts: DummyJsonPost[] } = await response.json();
+
+      setPosts(data.posts);
+    })();
+  }, []);
+
+  return (
+    <View style={styles.homeScreen}>
+      <FlatList
+        data={posts}
+        renderItem={({ item: post }) => <PostCard post={post} />}
+        ItemSeparatorComponent={() => <View style={styles.listSeparator} />}
+        keyExtractor={(post) => post.id.toString()}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  homeScreen: { flex: 1 },
+  listSeparator: { height: 16 },
+});
+
+export default Index;
