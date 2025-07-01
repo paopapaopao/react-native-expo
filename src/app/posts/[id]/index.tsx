e74ce8178be27ff18280d23fc9f739228206edb9
paopapaopao <paolo.casugay@gmail.com>
2025-07-02 00:47:09 +0800

    Create PostDetails screen

---
+++ src/app/posts/[id]/index.tsx
@@ -0,0 +1,32 @@
+import { useLocalSearchParams } from 'expo-router';
+import { JSX, useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import { PostCard } from '@/src/components';
+import type { DummyJsonPost } from '@/src/types';
+
+const Index = (): JSX.Element => {
+  const { id } = useLocalSearchParams();
+  const [post, setPost] = useState<DummyJsonPost | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`https://dummyjson.com/posts/${id}`);
+      const data: DummyJsonPost = await response.json();
+
+      setPost(data);
+    })();
+  }, []);
+
+  return (
+    <View style={styles.postDetailsScreen}>
+      <PostCard post={post} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  postDetailsScreen: { flex: 1 },
+});
+
+export default Index;
